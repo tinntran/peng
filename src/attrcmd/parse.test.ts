f75d1437bcef8fc1bdbcdf8f1fa4e8b3e291cf62
tinntran <tinn6w6@gmail.com>
2025-02-21 15:51:48 +0700

    add more tests

--- src/attrcmd/parse.test.ts
+++ src/attrcmd/parse.test.ts
@@ -1,15 +1,34 @@
 import { expect, test } from 'bun:test'
-import { parseCmd } from './parse'
+import { parseAttrCmd, parseCmd } from './parse'
+
 
 test('parse cmd', () => {
-  const inp = 'susy suss -n baka -t amongus -D :mmb'
+  const inp = 'susy suss -n baka -t amongus -D :mmb -boolean'
 
   expect(parseCmd(inp)).toEqual({
     positional: ['susy', 'suss'],
     flags: {
       n: 'baka',
       t: 'amongus',
-      D: ':mmb'
+      D: ':mmb',
+      boolean: 'true'
+    }
+  })
+})
+
+test('parse attr cmd', () => {
+  document.body.innerHTML = `<div id="test" p-test="pos_a pos_b -flag1 c -flag2 d -flag3 d"></div>`
+
+  const el = document.getElementById('test')
+
+  expect(el).not.toBeNil()
+
+  expect(parseAttrCmd(el!, 'p-test')).toEqual({
+    positional: ['pos_a', 'pos_b'],
+    flags: {
+      flag1: 'c',
+      flag2: 'd',
+      flag3: 'd'
     }
   })
 })
