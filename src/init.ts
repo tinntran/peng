98fc939e4d1831d1ffe2d16002269148802c9947
tinntran <tinn6w6@gmail.com>
2025-02-21 15:29:37 +0700

    add initialization log

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,8 @@
 import { Present, Slide } from './elements'
 
 export default function init() {
+  console.log('(")>\nYour penguin is ready to go!')
+
   customElements.define('p-present', Present)
   customElements.define('p-slide', Slide)
 }
