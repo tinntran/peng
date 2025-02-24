7eaa9eedb7bbc6586582b11ed4f940869879f497
tinntran <tinn6w6@gmail.com>
2025-02-24 14:17:38 +0700

    play animations subsequently

--- src/elements/slide.ts
+++ src/elements/slide.ts
@@ -4,7 +4,6 @@ import { generateAnims, parseAnims } from '../attrcmd'
 export default class Slide extends LitElement {
   anims: ReturnType<typeof parseAnims>
   animGroups: ReturnType<typeof generateAnims>
-  
 
   constructor() {
     super()
@@ -14,12 +13,22 @@ export default class Slide extends LitElement {
     this.animGroups = generateAnims(this.anims)
   }
 
+  playNextAnim() {
+    const current = this.animGroups.next()
+
+    if (current.value) {
+      current.value.map(anim => anim.restart())
+    }
+  }
+
   selected() {
-    this.anims.map(anim => anim.instance.restart())
+    this.animGroups = generateAnims(this.anims)
+
+    if (this.anims[0].start === 'withprev') this.playNextAnim()
   }
 
   unselected() {
-    this.anims.map(anim => anim.instance.pause())
+    this.anims.map(anim => anim.pause())
   }
 
   protected render() {
