6906db14ee073b75d37dab75afb9159907115fd4
tinntran <tinn6w6@gmail.com>
2025-03-01 13:31:19 +0700

    navigation restriction by animations

--- src/defaults/navigator.ts
+++ src/defaults/navigator.ts
@@ -2,16 +2,21 @@ import { parseAttrCmdForEvery } from '../attrcmd'
 import type { Present } from '../elements'
 
 export default function defaultNavigator(present: Present) {
-  let shouldNavigate = true
+  let intrClicked = true
 
   window.addEventListener('keydown', e => {
-    if ((e.key === 'ArrowRight' || e.key === 'Enter') && present.selectedIndex < present.slotNames.length - 1) {
-      present.selectedIndex++
+    if ((e.key === 'ArrowRight' || e.key === 'Enter')) {
+      const shouldNavigate = intrClicked
+        && present.getCurrentSlide()?.playNextAnim()
+        && present.selectedIndex < present.slotNames.length - 1
+
+      if (shouldNavigate)
+        present.selectedIndex++
     } else if (e.key === 'ArrowLeft' && present.selectedIndex > 0) {
       present.selectedIndex--
     }
 
-    shouldNavigate = true
+    intrClicked = true
   })
 
   parseAttrCmdForEvery<HTMLElement>(present, 'p-intr', (args, intr) => {
@@ -27,16 +32,21 @@ export default function defaultNavigator(present: Present) {
 
     intr.addEventListener('mousedown', e => {
       if (e.button === 0) {
-        shouldNavigate = !isIntr()
+        intrClicked = !isIntr()
       }
     })
   })
 
   window.addEventListener('mousedown', e => {
-    if (e.button === 0 && present.selectedIndex < present.slotNames.length - 1) {
-      if (shouldNavigate) present.selectedIndex++
+    if (e.button === 0) {
+      const shouldNavigate = intrClicked
+        && present.getCurrentSlide()?.playNextAnim()
+        && present.selectedIndex < present.slotNames.length - 1
+
+      if (shouldNavigate)
+        present.selectedIndex++
     }
 
-    shouldNavigate = true
+    intrClicked = true
   })
 }
